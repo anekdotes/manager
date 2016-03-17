f0ad5e90167c1739409d31f9c1dd0c20824624cb
Mathieu Gosselin <gosselin.mathieug@gmail.com>
2016-03-17 08:43:20 -0400

    First commit

---
+++ src/Image.php
@@ -0,0 +1,22 @@
+<?php namespace Anekdotes\Manager;
+
+class Image {
+  const GD = 1;
+  const IMAGICK = 2;
+  const GMAGICK = 3;
+
+  public static function open($file, $library = Image::GD) {
+    if ($library == Image::GD) {
+      $inst = new \Imagine\Gd\Imagine();
+    }
+    else if ($library == Image::IMAGICK) {
+      $inst = new \Imagine\Imagick\Imagine();
+    }
+    else if ($library == Image::GMAGICK) {
+      $inst = new \Imagine\Gmagick\Imagine();
+    }
+    
+    return $inst->open($file);
+  }
+
+}
