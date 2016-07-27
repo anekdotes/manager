5ad71cf73493344e589c99e755c4fc933e686d01
Mathieu Gosselin <gosselin.mathieug@gmail.com>
2016-07-27 15:42:50 -0400

    improved manager and improved test

--- src/anekdotes/Manager.php
+++ src/anekdotes/Manager.php
@@ -10,14 +10,15 @@ use Imagine\Image\ImageInterface;
 class Manager
 {
     const IMAGES = ['jpg', 'jpeg', 'png'];
-    const DOCUMENTS = ['pdf', 'doc', 'docx'];
-    protected $exts = ['jpg', 'png', 'jpeg'];
-    protected $path = 'uploads/';
-    protected $weight = 3000000;
-    protected $size = null;
+    const DOCUMENTS = ['pdf', 'doc', 'docx', 'xls', 'xlsx', 'txt'];
     protected $prefix = 'public/';
+    protected $exts = ['jpg', 'png', 'jpeg'];
+    protected $weight = 3000000;
+    protected $path = 'uploads/';
+    protected $size = null;
     protected $quality = 70;
     public $success = false;
+    public $errors = [];
 
   /**
    * overwrite default manager's properties.
@@ -26,20 +27,21 @@ class Manager
    */
   public function __construct($options = [])
   {
+      $this->errors = [];
+      if (isset($options['prefix'])) {
+          $this->prefix = $options['prefix'];
+      }
       if (isset($options['exts'])) {
           $this->exts = $options['exts'];
-      }
-      if (isset($options['path'])) {
-          $this->path = $options['path'];
       }
       if (isset($options['weight'])) {
           $this->weight = $options['weight'];
       }
+      if (isset($options['path'])) {
+          $this->path = $options['path'];
+      }
       if (isset($options['size'])) {
           $this->size = $options['size'];
-      }
-      if (isset($options['prefix'])) {
-          $this->prefix = $options['prefix'];
       }
   }
 
@@ -67,39 +69,69 @@ class Manager
   public function manage($fileInfo, Closure $uploadCallback = null)
   {
       if (gettype($fileInfo) != 'array') {
-          throw new \Exception('First parameter must be an `array` and not a `'.gettype($fileInfo).'`');
+          $this->success = false;
+          $this->errors[] = 'First parameter must be an `array` and not a `'.gettype($fileInfo).'`';
+          return;
+      }
+
+      if (empty($fileInfo)) {
+          $this->success = false;
+          $this->errors[] = 'Array cannot be empty';
+          return;
+      }
+
+      if (!isset($fileInfo['error'])) {
+          $this->success = false;
+          $this->errors[] = 'Array must be a valid HTTP_POST_FILES ';
+          return;
       }
 
       if (is_null($fileInfo['error']) || $fileInfo['error'] > 0) {
-          throw new \Exception('Invalid file');
+          $this->success = false;
+          $this->errors[] = 'Invalid file';
+          return;
       }
 
       $fileInfo = array_merge($fileInfo, pathinfo($fileInfo['name']));
+
+      if (!isset($fileInfo['extension'])) {
+          $this->success = false;
+          $this->errors[] = 'Invalid file';
+          return;
+      }
+
       $fileInfo['extension'] = strtolower($fileInfo['extension']);
-      $filename = date('YmdHis', time()).'.jpg';
+      $filename = date('YmdHis', time()).'.'.$fileInfo['extension'];
 
       if ($fileInfo['size'] / 1000000 > $this->weight) {
-          throw new \Exception('File is too big');
+          $this->success = false;
+          $this->errors[] = 'File is too big';
+          return;
       }
 
       if (!in_array($fileInfo['extension'], $this->exts)) {
-          throw new \Exception('File extension is not supported');
+          $this->success = false;
+          $this->errors[] = 'File extension is not supported';
+          return;
       }
 
-    //there's a callback, execute the callback and return its value as new filename
-    if ($uploadCallback) {
-        $filename = $uploadCallback($fileInfo);
-    }
+      //there's a callback, execute the callback and return its value as new filename
+      if ($uploadCallback) {
+          $filename = $uploadCallback($fileInfo);
+      }
 
       if (in_array($fileInfo['extension'], $this::IMAGES)) {
           $this->handleImage($fileInfo, $filename);
       } elseif (in_array($fileInfo['extension'], $this::DOCUMENTS)) {
           $this->handleDocument($fileInfo, $filename);
       } else {
-          $this->handleOther($fileInfo, $filename);
+          $this->success = false;
+          $this->errors[] = 'File extension is not supported';
+          return;
       }
 
-      return true;
+      $this->success = true;
+      return;
   }
 
   /**
@@ -137,35 +169,35 @@ class Manager
               $newPath = $this->prefix.$newPath.$filename;
               $quality = isset($config['quality']) ? $config['quality'] : $this->quality;
               $file = Image::open($fileInfo['tmp_name']);
-        //
-        $size = $file->getSize();
+              $size = $file->getSize();
+
               switch ($config['resize']) {
-          case 'widen':
-            $file = $file->resize($size->widen($config['width']));
-            break;
-          case 'heighten':
-            $file = $file->resize($size->heighten($config['height']));
-            break;
-          case 'max':
-            if ($size->getWidth() > $config['width']) {
-                $file = $file->resize($size->widen($config['width']));
-            }
-            if ($size->getHeight() > $config['height']) {
-                $file = $file->resize($size->heighten($config['height']));
-            }
-          break;
-        }
+                  case 'widen':
+                      $file = $file->resize($size->widen($config['width']));
+                      break;
+                  case 'heighten':
+                      $file = $file->resize($size->heighten($config['height']));
+                      break;
+                  case 'max':
+                      if ($size->getWidth() > $config['width']) {
+                          $file = $file->resize($size->widen($config['width']));
+                      }
+                      if ($size->getHeight() > $config['height']) {
+                          $file = $file->resize($size->heighten($config['height']));
+                      }
+                  break;
+              }
+
               if ($config['crop']) {
                   $size = new Box($config['width'], $config['height']);
                   $mode = ImageInterface::THUMBNAIL_OUTBOUND;
                   $file = $file->thumbnail($size, $mode);
               }
-        //
-        $tmpPath = $fileInfo['tmp_name'].'.'.$fileInfo['extension'];
-              $file->save($tmpPath, [
-            'quality' => $quality,
-        ]);
 
+              $tmpPath = $fileInfo['tmp_name'].'.'.$fileInfo['extension'];
+              $file->save($tmpPath, [
+                  'quality' => $quality,
+              ]);
               File::move($tmpPath, $newPath);
           }
       }
@@ -181,19 +213,6 @@ class Manager
   {
       self::directorize($this->path);
       $newPath = $this->prefix.$this->path.$filename;
-      File::move($fileInfo['tmp_name'], $newPath);
-  }
-
-  /**
-   * Handle all other file format other than IMAGES and DOCUMENTS array.
-   *
-   * @param  string $fileInfo uploaded file information the same as $_FILE['input name'] + pathinfo['file name']
-   * @param  string $filename filename to save the new file
-   */
-  private function handleOther($fileInfo, $filename)
-  {
-      self::directorize($this->path);
-      $newPath = $this->prefix.$this->path.$filename;
-      File::move($fileInfo['tmp_name'], $newPath);
+      File::copy($fileInfo['tmp_name'], $newPath);
   }
 }
