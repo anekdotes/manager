4f3d12f46154f331c7969d8249e120f3e4922967
Mathieu Gosselin <gosselin.mathieug@gmail.com>
2016-07-27 16:12:28 -0400

    fix styleci v2

--- tests/ManagerTest.php
+++ tests/ManagerTest.php
@@ -43,28 +43,28 @@ class ManagerTest extends PHPUnit_Framework_TestCase
         $m = new Manager();
         $m->set('misc', true);
         try {
-          $reflection = new \ReflectionClass($m);
-          $reflectionProperty = $reflection->getProperty('misc');
+            $reflection = new \ReflectionClass($m);
+            $reflectionProperty = $reflection->getProperty('misc');
         } catch (\Exception $e) {
-          $this->assertInstanceOf(Manager::class, $m);
+            $this->assertInstanceOf(Manager::class, $m);
         }
     }
 
     public function testManager5()
     {
         $configs = [
-          'prefix' => '',
-          'exts'   => ['jpg', 'jpeg', 'png', 'gif'],
-          'weight'   => 3000000,
-          'path'   => 'tests/cache/',
-          'size'   => [
-            'square' => [
-              'resize' => 'heighten',
-              'crop'   => true,
-              'width'  => 200,
-              'height' => 200,
+            'prefix' => '',
+            'exts'   => ['jpg', 'jpeg', 'png', 'gif'],
+            'weight'   => 3000000,
+            'path'   => 'tests/cache/',
+            'size'   => [
+                'square' => [
+                    'resize' => 'heighten',
+                    'crop'   => true,
+                    'width'  => 200,
+                    'height' => 200,
+                ],
             ],
-          ],
         ];
         $m = new Manager($configs);
         $reflection = new \ReflectionClass($m);
@@ -76,48 +76,48 @@ class ManagerTest extends PHPUnit_Framework_TestCase
 
     public function testManager6()
     {
-      $m = new Manager();
-      $m->manage('');
-      $this->assertFalse($m->success);
+        $m = new Manager();
+        $m->manage('');
+        $this->assertFalse($m->success);
     }
 
     public function testManager7()
     {
-      $m = new Manager();
-      $m->manage(1);
-      $this->assertFalse($m->success);
+        $m = new Manager();
+        $m->manage(1);
+        $this->assertFalse($m->success);
     }
 
     public function testManager8()
     {
-      $m = new Manager();
-      $m->manage((object) []);
-      $this->assertFalse($m->success);
+        $m = new Manager();
+        $m->manage((object) []);
+        $this->assertFalse($m->success);
     }
 
     public function testManager9()
     {
-      $m = new Manager();
-      $m->manage('');
-      $this->assertTrue(count($m->errors) > 0);
+        $m = new Manager();
+        $m->manage('');
+        $this->assertTrue(count($m->errors) > 0);
     }
 
     public function testManager10()
     {
-      $m = new Manager();
-      $m->manage([]);
-      $this->assertFalse($m->success);
+        $m = new Manager();
+        $m->manage([]);
+        $this->assertFalse($m->success);
     }
 
     public function testManager11()
     {
-      $m = new Manager();
-      $gibberish = [
-          'foo' => 'bar',
-          'poo' => 'bar'
-      ];
-      $m->manage($gibberish);
-      $this->assertFalse($m->success);
+        $m = new Manager();
+        $gibberish = [
+            'foo' => 'bar',
+            'poo' => 'bar',
+        ];
+        $m->manage($gibberish);
+        $this->assertFalse($m->success);
     }
 
     public function testManager12()
@@ -136,197 +136,197 @@ class ManagerTest extends PHPUnit_Framework_TestCase
 
     public function testManager13()
     {
-      $m = new Manager();
-      $dummies = [
-          'name'     => 'foo',
-          'type'     => 'foo',
-          'tmp_name' => 'foo',
-          'error'    => 4,
-          'size'     => 1,
-      ];
-      $m->manage($dummies);
-      $this->assertFalse($m->success);
+        $m = new Manager();
+        $dummies = [
+            'name'     => 'foo',
+            'type'     => 'foo',
+            'tmp_name' => 'foo',
+            'error'    => 4,
+            'size'     => 1,
+        ];
+        $m->manage($dummies);
+        $this->assertFalse($m->success);
     }
 
     public function testManager14()
     {
-      $configs = [
-          'weight'   => 0.02,
-      ];
-      $path = __dir__.'/dummy/dummy.jpg';
-      $m = new Manager($configs);
-      $dummies = [
-          'name'     => $path,
-          'type'     => mime_content_type($path),
-          'tmp_name' => $path,
-          'error'    => 0,
-          'size'     => File::size($path),
-      ];
-      $m->manage($dummies);
-      $this->assertFalse($m->success);
+        $configs = [
+            'weight'   => 0.02,
+        ];
+        $path = __dir__.'/dummy/dummy.jpg';
+        $m = new Manager($configs);
+        $dummies = [
+            'name'     => $path,
+            'type'     => mime_content_type($path),
+            'tmp_name' => $path,
+            'error'    => 0,
+            'size'     => File::size($path),
+        ];
+        $m->manage($dummies);
+        $this->assertFalse($m->success);
     }
 
     public function testManager15()
     {
-      $configs = [
-          'weight'   => 3000000,
-          'exts'   => ['jpg', 'jpeg', 'png', 'gif'],
-      ];
-      $path = __dir__.'/dummy/dummy.swf';
-      $m = new Manager($configs);
-      $dummies = [
-          'name'     => $path,
-          'type'     => mime_content_type($path),
-          'tmp_name' => $path,
-          'error'    => 0,
-          'size'     => File::size($path),
-      ];
-      $m->manage($dummies);
-      $this->assertFalse($m->success);
+        $configs = [
+            'weight'   => 3000000,
+            'exts'   => ['jpg', 'jpeg', 'png', 'gif'],
+        ];
+        $path = __dir__.'/dummy/dummy.swf';
+        $m = new Manager($configs);
+        $dummies = [
+            'name'     => $path,
+            'type'     => mime_content_type($path),
+            'tmp_name' => $path,
+            'error'    => 0,
+            'size'     => File::size($path),
+        ];
+        $m->manage($dummies);
+        $this->assertFalse($m->success);
     }
 
     public function testManager16()
     {
-      $configs = [
-          'weight'   => 3000000,
-          'exts'   => ['jpg', 'jpeg', 'png', 'gif'],
-      ];
-      $m = new Manager($configs);
-      $dummies = [
-          'name'     => 'foo',
-          'type'     => 'foo',
-          'tmp_name' => 'foo',
-          'error'    => 0,
-          'size'     => 100,
-      ];
-      $m->manage($dummies);
-      $this->assertFalse($m->success);
+        $configs = [
+            'weight'   => 3000000,
+            'exts'   => ['jpg', 'jpeg', 'png', 'gif'],
+        ];
+        $m = new Manager($configs);
+        $dummies = [
+            'name'     => 'foo',
+            'type'     => 'foo',
+            'tmp_name' => 'foo',
+            'error'    => 0,
+            'size'     => 100,
+        ];
+        $m->manage($dummies);
+        $this->assertFalse($m->success);
     }
 
     public function testManager17()
     {
-      $configs = [
-          'weight'   => 3000000,
-          'exts'   => ['jpg', 'jpeg', 'png', 'gif'],
-      ];
-      $m = new Manager($configs);
-      $dummies = [
-          'name'     => 'foo',
-          'type'     => 'foo',
-          'tmp_name' => 'foo',
-          'error'    => 0,
-          'size'     => 100,
-      ];
-      $m->manage($dummies);
-      $this->assertFalse($m->success);
+        $configs = [
+            'weight'   => 3000000,
+            'exts'   => ['jpg', 'jpeg', 'png', 'gif'],
+        ];
+        $m = new Manager($configs);
+        $dummies = [
+            'name'     => 'foo',
+            'type'     => 'foo',
+            'tmp_name' => 'foo',
+            'error'    => 0,
+            'size'     => 100,
+        ];
+        $m->manage($dummies);
+        $this->assertFalse($m->success);
     }
 
     public function testManager18()
     {
-      $configs = [
-          'prefix' => '',
-          'exts'   => ['jpg', 'jpeg', 'png', 'gif'],
-          'weight'   => 3000000,
-          'path'   => 'tests/cache/',
-          'size'   => [
-              'square' => [
-                  'resize' => 'heighten',
-                  'crop'   => true,
-                  'width'  => 200,
-                  'height' => 200,
-              ],
-          ],
-      ];
-      $path = __dir__.'/dummy/dummy.jpg';
-      $dummies = [
-          'name'     => $path,
-          'type'     => mime_content_type($path),
-          'tmp_name' => $path,
-          'error'    => 0,
-          'size'     => File::size($path),
-      ];
+        $configs = [
+            'prefix' => '',
+            'exts'   => ['jpg', 'jpeg', 'png', 'gif'],
+            'weight'   => 3000000,
+            'path'   => 'tests/cache/',
+            'size'   => [
+                'square' => [
+                    'resize' => 'heighten',
+                    'crop'   => true,
+                    'width'  => 200,
+                    'height' => 200,
+                ],
+            ],
+        ];
+        $path = __dir__.'/dummy/dummy.jpg';
+        $dummies = [
+            'name'     => $path,
+            'type'     => mime_content_type($path),
+            'tmp_name' => $path,
+            'error'    => 0,
+            'size'     => File::size($path),
+        ];
 
-      $m = new Manager($configs);
-      $m->manage($dummies);
-      $this->assertTrue($m->success);
+        $m = new Manager($configs);
+        $m->manage($dummies);
+        $this->assertTrue($m->success);
     }
 
     public function testManager19()
     {
-      $configs = [
-          'prefix' => '',
-          'exts'   => ['jpg', 'jpeg', 'png', 'gif'],
-          'weight'   => 3000000,
-          'path'   => 'tests/cache/',
-          'size'   => [
-              'square' => [
-                  'resize' => 'heighten',
-                  'crop'   => true,
-                  'width'  => 200,
-                  'height' => 200,
-              ],
-          ],
-      ];
-      $path = __dir__.'/dummy/dummy.jpg';
-      $dummies = [
-          'name'     => $path,
-          'type'     => mime_content_type($path),
-          'tmp_name' => $path,
-          'error'    => 0,
-          'size'     => File::size($path),
-      ];
+        $configs = [
+            'prefix' => '',
+            'exts'   => ['jpg', 'jpeg', 'png', 'gif'],
+            'weight'   => 3000000,
+            'path'   => 'tests/cache/',
+            'size'   => [
+                'square' => [
+                    'resize' => 'heighten',
+                    'crop'   => true,
+                    'width'  => 200,
+                    'height' => 200,
+                ],
+            ],
+        ];
+        $path = __dir__.'/dummy/dummy.jpg';
+        $dummies = [
+            'name'     => $path,
+            'type'     => mime_content_type($path),
+            'tmp_name' => $path,
+            'error'    => 0,
+            'size'     => File::size($path),
+        ];
 
-      $m = new Manager($configs);
-      $m->manage($dummies, function () {
-          return mt_rand(1000, 100000).'.jpg';
-      });
-      $this->assertTrue($m->success);
+        $m = new Manager($configs);
+        $m->manage($dummies, function () {
+            return mt_rand(1000, 100000).'.jpg';
+        });
+        $this->assertTrue($m->success);
     }
 
     public function testManager20()
     {
-      $configs = [
-          'prefix' => '',
-          'exts'   => ['doc', 'docx', 'pdf'],
-          'weight'   => 3000000,
-          'path'   => 'tests/cache/',
-          'size'   => [],
-      ];
-      $path = __dir__.'/dummy/dummy.doc';
-      $dummies = [
-          'name'     => $path,
-          'type'     => mime_content_type($path),
-          'tmp_name' => $path,
-          'error'    => 0,
-          'size'     => File::size($path),
-      ];
+        $configs = [
+            'prefix' => '',
+            'exts'   => ['doc', 'docx', 'pdf'],
+            'weight'   => 3000000,
+            'path'   => 'tests/cache/',
+            'size'   => [],
+        ];
+        $path = __dir__.'/dummy/dummy.doc';
+        $dummies = [
+            'name'     => $path,
+            'type'     => mime_content_type($path),
+            'tmp_name' => $path,
+            'error'    => 0,
+            'size'     => File::size($path),
+        ];
 
-      $m = new Manager($configs);
-      $m->manage($dummies);
-      $this->assertTrue($m->success);
+        $m = new Manager($configs);
+        $m->manage($dummies);
+        $this->assertTrue($m->success);
     }
 
     public function testManager21()
     {
-      $configs = [
-          'prefix' => '',
-          'exts'   => ['swf', 'fla'],
-          'weight'   => 3000000,
-          'path'   => 'tests/cache/',
-          'size'   => [],
-      ];
-      $path = __dir__.'/dummy/dummy.swf';
-      $dummies = [
-          'name'     => $path,
-          'type'     => mime_content_type($path),
-          'tmp_name' => $path,
-          'error'    => 0,
-          'size'     => File::size($path),
-      ];
+        $configs = [
+            'prefix' => '',
+            'exts'   => ['swf', 'fla'],
+            'weight'   => 3000000,
+            'path'   => 'tests/cache/',
+            'size'   => [],
+        ];
+        $path = __dir__.'/dummy/dummy.swf';
+        $dummies = [
+            'name'     => $path,
+            'type'     => mime_content_type($path),
+            'tmp_name' => $path,
+            'error'    => 0,
+            'size'     => File::size($path),
+        ];
 
-      $m = new Manager($configs);
-      $m->manage($dummies);
-      $this->assertFalse($m->success);
+        $m = new Manager($configs);
+        $m->manage($dummies);
+        $this->assertFalse($m->success);
     }
 
     public function testManager22()
@@ -363,33 +363,33 @@ class ManagerTest extends PHPUnit_Framework_TestCase
 
     public function testManager23()
     {
-      $configs = [
-          'prefix' => '',
-          'exts'   => ['jpg', 'jpeg', 'png', 'gif'],
-          'weight'   => 3000000,
-          'path'   => 'tests/cache/',
-          'size'   => [
-              'square' => [
-                  'resize' => 'max',
-                  'crop'   => true,
-                  'width'  => 200,
-                  'height' => 200,
-              ],
-          ],
-      ];
-      $path = __dir__.'/dummy/dummy.jpg';
-      $dummies = [
-          'name'     => $path,
-          'type'     => mime_content_type($path),
-          'tmp_name' => $path,
-          'error'    => 0,
-          'size'     => File::size($path),
-      ];
+        $configs = [
+            'prefix' => '',
+            'exts'   => ['jpg', 'jpeg', 'png', 'gif'],
+            'weight'   => 3000000,
+            'path'   => 'tests/cache/',
+            'size'   => [
+                'square' => [
+                    'resize' => 'max',
+                    'crop'   => true,
+                    'width'  => 200,
+                    'height' => 200,
+                ],
+            ],
+        ];
+        $path = __dir__.'/dummy/dummy.jpg';
+        $dummies = [
+            'name'     => $path,
+            'type'     => mime_content_type($path),
+            'tmp_name' => $path,
+            'error'    => 0,
+            'size'     => File::size($path),
+        ];
 
-      $m = new Manager($configs);
-      $m->manage($dummies, function () {
-          return mt_rand(1000, 100000).'.jpg';
-      });
-      $this->assertTrue($m->success);
+        $m = new Manager($configs);
+        $m->manage($dummies, function () {
+            return mt_rand(1000, 100000).'.jpg';
+        });
+        $this->assertTrue($m->success);
     }
 }
